3b71f061be689aa731860c63a0f7fd04704e2c1d
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-07-17 19:34:03 -0500

    start of 12.2.4

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
+//connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    //your mysql username,
+    user: 'root',
+    //your mysql password
+    password: "@UhaDk7yj",
+    database: 'election'
+  },
+  console.log('Connected to the election database')
+);
 
 // default response for any other request (not found)
 app.use((req,res) => {
