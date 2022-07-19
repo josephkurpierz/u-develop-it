59610858c0557b8400ad113ad1a5296ea0ff4358
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-07-18 19:25:14 -0500

    query statements

--- server.js
+++ server.js
@@ -19,6 +19,33 @@ const db = mysql.createConnection(
   console.log('Connected to the election database')
 );
 
+// const sql = `INSERT INTO candidates(id,first_name,last_name,industry_connected) VALUES (?,?,?,?)`;
+// const params = [1,'Ronald', 'Firbank',1];
+// db.query(sql,params,(err,result)=>{
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// db.query(`DELETE FROM candidates Where id=?`,1,(err,result)=>{
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// db.query(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// db.query(`SELECT * FROM candidates`, (err,rows)=>{
+//   console.log(rows);
+// })
+
 // default response for any other request (not found)
 app.use((req,res) => {
   res.status(404).end();
